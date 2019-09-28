1b8b6208a5f641930732333aef20158e5000a41a
natafmc <nata.medc@gmail.com>
2019-09-27 20:39:01 -0400

    MEJORAS EN ELASTIC - dsps de la entrega

--- ApiRestFull/index.js
+++ ApiRestFull/index.js
@@ -8,7 +8,11 @@ const neo4jDate = require('neo4j-driver').Date
 const method = require('method-override')
 const solr = require('solr-node')
 const influx = require('influxdb-nodejs')
-var people = require('./people.json')
+
+const elastic = require('@elastic/elasticsearch')
+
+
+//var people = require('./people.json')
 
 
 const dbConfig = require('./src/config/database.config.js')
@@ -17,8 +21,6 @@ const dbConfig = require('./src/config/database.config.js')
 const app = express()
 
 // DATABASE
-
-
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -39,49 +41,72 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(method('method'));
 
-// ROUTES
-// app.get('/', (req, res) => {
-//     session.run('MATCH (m:Movie {title:"Avengers"}) RETURN m')
-//         .then((result) => {
-//             var movies = []
-//             result.records.forEach((record) => {
-//                 //console.log(record._fields[0].properties)
-//                 movies.push({
-
-//                     //id: record._fields[0].properties.identy.low,
-//                     title: record._fields[0].properties.title
-//                 })
-//                 //console.log(record._fields[0].properties)
-//             })
-
-//             session.run('MATCH (Person {name: "Cantinflas"}) RETURN Person')
-//                 .then((result2) => {
-//                     var actors = []
-//                     result2.records.forEach((record) => {
-//                         console.log(record._fields[0].properties)
-//                         actors.push({
-//                             name: record._fields[0].properties.name
-//                         })
-
-//                     })
-
-//                     res.render('index', { actors, movies })
-//                 })
-//                 .catch((err) => {
-//                     console.log(err)
-//                 })
-
-//         })
+var client = new elastic.Client({
+    node: 'http://localhost:9200'
+})
 
 
-//         .catch((err) => {
-//             console.log(err)
-//         })
-//     //res.send('It works')
-// })
+/** */
+async function run() {
+    // Let's start by indexing some data
+    // await client.index({
+    //     index: 'denis',
+    //     // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    //     body: {
+    //         character: 'Ned Stark',
+    //         quote: 'Winter is coming.'
+    //     }
+    // })
 
-//app.post('/kudos/add', )
+    // await client.index({
+    //     index: 'game-of-thrones',
+    //     // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    //     body: {
+    //         character: 'Daenerys Targaryen',
+    //         quote: 'I am the blood of the dragon.'
+    //     }
+    // })
 
+    // await client.index({
+    //     index: 'game-of-thrones',
+    //     // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    //     body: {
+    //         character: 'Tyrion Lannister',
+    //         quote: 'A mind needs books like a sword needs a whetstone.'
+    //     }
+    // })
+    // await client.index({
+    //     index: 'denis',
+    //     // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    //     body: {
+    //         character: 'Goku Lannister',
+    //         quote: 'A mind needs winther yamcha a sword needs a whetstone.'
+    //     }
+    // })
+
+    // // here we are forcing an index refresh, otherwise we will not
+    // // get any result in the consequent search
+    await client.indices.refresh({ index: 'game-of-thrones' })
+
+    // Let's search!
+    const { body } = await client.search({
+        index: 'game-of-thrones',
+        // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+        body: {
+            query: {
+                fuzzy: { character: 'Tierion' }
+            }
+        }
+    })
+
+    console.log(body.hits.hits)
+}
+run().catch(console.log)
+
+/** */
+app.get('/', (req, res) => {
+    res.send('Its works!!!')
+})
 require('./src/routes/kudos.routes.js')(app)
 require('./src/routes/user.routes.js')(app)
 
